1dc4da8c5025bbfa3232a4cc1f52349708258469
TinoTheCoder <justinod408@gmail.com>
2026-09-17 09:34:19 +0200

    Updated AI URL to live Render server

--- backend/routes/listingRoutes.js
+++ backend/routes/listingRoutes.js
@@ -74,7 +74,7 @@ router.post('/', auth, (req, res) => {
 
         console.log("🤖 Asking AI for clearance...");
         // Change the port from 5000 to 5001
-        const aiResponse = await axios.post('http://localhost:5001/evaluate_listing', aiPayload);
+        const aiResponse = await axios.post('https://marketplace-ai-scamdetector.onrender.com/evaluate_listing', aiPayload);
         const riskData = aiResponse.data;
 
         aiScore = riskData.metadata_risk_score || 0;
